21b2a4047fee8de134f60c0a79fe2f82a4e79df9
张波 <604475943@qq.com>
2026-07-01 21:06:36 +0800

    init commit

---
+++ main.ts
@@ -0,0 +1,26 @@
+import { join } from "deno://std/path"
+
+Deno.serve(async (request: Request): Promise<Response> => {
+    const { pathname } = new URL(request.url)
+    if (!/files\/*.json/.test(pathname)) {
+        return new Response("不是有效的文件路径", { status: 403 })
+    }
+
+    const filePath = join(Deno.cwd(), pathname)
+    try {
+        const fileContent = await Deno.readTextFile(filePath)
+        return new Response(fileContent, {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+    }
+    catch (error) {
+        if (error instanceof Deno.errors.NotFound) {
+            return new Response("File not found", { status: 404 })
+        } else {
+            return new Response("Internal Server Error", { status: 500 })
+        }
+    }
+})
